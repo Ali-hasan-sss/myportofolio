1e990e012457afe2d310588cc05b1fd6d7667e28
Ali-Hasan-sss <emonate8@gmail.com>
2025-02-20 17:16:29 +0300

    add neworder page

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,24 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+module.exports = {
+  async headers() {
+    return [
+      {
+        source: "/api/:path*", // السماح بجميع API Routes
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value:
+              "https://alitech-7sexcrwyd-ali-hasan-sss-projects.vercel.app",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT, DELETE",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization",
+          },
+        ],
+      },
+    ];
+  },
 };
-
-export default nextConfig;
